eeeea863b115eecdcc3fd920dc45d39b9fd2628b
Simone Basile <simonebasile199@gmail.com>
2023-01-24 17:56:09 +0100

    Update es1.js

--- es1.js
+++ es1.js
@@ -7,17 +7,17 @@ class Person {
 
   static whoIsOlder(p1, p2) {
     if (p1.age > p2.age) {
-      console.log(p1.name + "è più grande di" + p2.name);
+      console.log(`${p1.name} è più grande di ${p2.name}`);
     } else if (p2.age > p1.age) {
-      console.log(p2.name, "è più grande di", p1.name);
+      console.log(`${p2.name} è più grande di ${p1.name}`);
     } else {
-      console.log(p1.name, "e", p2.name, "hanno la stessa età");
+      console.log(`${p1.name} e ${p1.name} hanno la stessa età`);
     }
   }
 }
 
 let personeACaso = [
-  new Person("Aragorn", 71),
+  new Person("Aragorn", 64),
   new Person("Legolas", 271),
   new Person("Ghimli", 203),
 ];
